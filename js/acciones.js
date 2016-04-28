197b4a7dce77c33372f94f4332796163da8b55a9
rojanomartinez <jesus.3fdsaupoo@gmail.com>
2016-04-28 14:35:16 -0500

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener( 'deviceready', function() {
 		$('#btnvibrar').on('tap',function() {
-			navigator.notification.vibrate(2000);
+			navigator.notification.vibrate(30000);
     });
   });
 });
